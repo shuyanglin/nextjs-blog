433a146881541878cb4b73363ba1106a9fbf85ed
shuyanglin <shuyanglin.tw@gmail.com>
2021-11-21 14:38:02 +0000

    name changes

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Shu Yang Lin'
-export const siteTitle = 'Shu\'s blog'
+const name = 'S. Yang'
+export const siteTitle = 'S\'s blog'
 
 export default function Layout({ children, home }) {
   return (
